f30e402da0f5a6c0211fc44047fab1f034235d0b
luyen luu <luyenluu003@gmail.com>
2025-04-10 21:21:48 +0700

    update origin

--- security/src/main/java/com/alibou/security/config/WebConfig.java
+++ security/src/main/java/com/alibou/security/config/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173") // Cho phép React truy cập
+                        .allowedOrigins("http://localhost:5173", "http://localhost:8081")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
